43bc71c3e7863ade6d97e629339408d3fd126054
Jonied mirza shakib <MDSHAKIB01088@gmail.com>
2022-07-25 19:29:42 +0600

    complete the work backend

--- index.js
+++ index.js
@@ -41,6 +41,34 @@ async function run() {
     const profileCollection = client.db("food").collection("profile")
     const userCollection = client.db("food").collection("user")
 
+    app.get('/user',verifyJWT, async (req, res) => {
+      const query = {};
+      const result = await userCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.get('/admin/:email',verifyJWT, async (req, res) => {
+      const email=req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin= user?.role==='admin';
+      res.send({admin:isAdmin})
+    })
+
+    app.put('/user/admin/:email',verifyJWT, async(req,res)=>{
+      const email=req.params.email;
+      const requester=req.decoded.email;
+      const requesterAccount= await userCollection.findOne({email: requester});
+      if(requesterAccount.role==='admin'){
+        const filter={email: email};
+        const updateDoc = {
+            $set: {role:'admin'},
+          };
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result)
+      }else{
+        res.status(403).send({message:'Forbidden Access'})
+      }
+  })
     app.put('/user/:email',async(req,res)=>{
       const email=req.params.email;
       const user=req.body;
@@ -53,11 +81,14 @@ async function run() {
         const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' });
         res.send({result,token})
   })
-  app.get('/user', async (req, res) => {
-    const query = {};
-    const result = await userCollection.find(query).toArray();
+
+  app.delete('/user/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id: ObjectId(id)};
+    const result= await userCollection.deleteOne(query);
     res.send(result)
   })
+  
     app.post('/breakfast',async(req,res)=>{
       const breakfast=req.body;
       const result= await breakfastCollection.insertOne(breakfast);
@@ -240,6 +271,7 @@ async function run() {
       const result= await profileCollection.insertOne(query);
       res.send(result)
     })
+    
     app.get('/profile',async(req,res)=>{
       const query={};
       const result= await profileCollection.find(query).toArray();
